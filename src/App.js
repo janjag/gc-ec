c8c047cfd550908bfab5ea36f95f5e051ec0bc7b
Jan Jagieła <janjag@humanistcanco.de>
2018-05-29 13:11:23 +0200

    basic auth functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Aux from './hoc/Aux';
+import Auth from './containers/Auth/Auth';
+import Calendars from './containers/Calendars/Calendars';
 
 class App extends Component {
+
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/calendars" component={Calendars} />
+        <Route path="/" exact component={Auth} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
     return (
-      <div className="App">
-      Hello World
-      </div>
+      <Aux>
+        {routes}
+      </Aux>
     );
   }
 }
---
+++ src/containers/Calendars/Calendars.js
@@ -0,0 +1,11 @@
+import React, { Component }  from 'react';
+
+class Calendars extends Component {
+    render () {
+        return (
+            <h1>Yours Calendars</h1>
+        );
+    }
+} 
+
+export default Calendars;
